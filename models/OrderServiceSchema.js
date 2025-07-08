8553af5c9edaf079ed18b949cd2a1f715944e53a
OxeAlex23 <souzaalexciano@gmail.com>
2025-07-08 17:01:20 -0300

    Update OrderServiceSchema.js

--- models/OrderServiceSchema.js
+++ models/OrderServiceSchema.js
@@ -1,9 +1,33 @@
 import mongoose from "mongoose";
+import { calculateServiceTotal } from "../routes/calculateServiceTotal.js";
 
 const OrderServiceSchema = new mongoose.Schema({
     OrderId: {type: mongoose.Types.ObjectId, ref: "Order", required: true},
     ServiceId: {type: mongoose.Types.ObjectId, ref: "Services", required: true},
     Quantity: Number
+});
+
+
+OrderServiceSchema.post('save', async function (doc, next) {
+  await calculateServiceTotal(doc.OrderId);
+  next();
+});
+
+OrderServiceSchema.post('findOneAndUpdate', async function (doc, next) {
+  if (doc) {
+    await calculateServiceTotal(doc.OrderId);
+  }
+  next();
+});
+
+OrderServiceSchema.post('insertMany', async function (docs) {
+  if (docs.length > 0) {
+    await calculateServiceTotal(docs[0].OrderId);
+  }
+});
+
+OrderServiceSchema.post('deleteOne', { document: true, query: false }, async function () {
+  await calculateServiceTotal(this.OrderId);
 });
 
 export default mongoose.model('OrderService', OrderServiceSchema);
