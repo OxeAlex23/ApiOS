7ab5730e20c0a676c0f869d42490cb3ddb908be0
OxeAlex23 <souzaalexciano@gmail.com>
2025-07-13 01:10:36 -0300

    cors

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ import orderStatusRoutes from './routes/orderStatus.routes.js';
 import businessUserRoleRoutes from './routes/businessUserRole.routes.js';
 import businessUserRoutes from './routes/businessUser.routes.js';
 import userTypeRoutes from './routes/userType.routes.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/orderProducts', orderProductRoutes);
