72135405cfb31e00ff9a443aec3d90b316cf7286
OxeAlex23 <souzaalexciano@gmail.com>
2025-07-13 01:22:03 -0300

    Update swagger.js

---
+++ swagger.js
@@ -0,0 +1,22 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API OS",
+      version: "1.0.0",
+      description: "Documentação da sua API",
+    },
+  },
+  apis: ["./routes/*.js"], // caminhos dos arquivos com comentários Swagger
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+function setupSwagger(app) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+module.exports = setupSwagger;
