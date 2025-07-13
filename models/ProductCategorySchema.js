1b1d210d23829698e3f3d4bf2addae5cba729a55
OxeAlex23 <souzaalexciano@gmail.com>
2025-07-13 02:57:22 -0300

    Update ProductCategorySchema.js

--- models/ProductCategorySchema.js
+++ models/ProductCategorySchema.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const ProductCategorySchema = new mongoose.Schema({
     ProductCategoryDesc: String,
-    BusinessId: {type: mongoose.Types.ObjectId, ref: 'Business', required: true}
+    BusinessId: {type: mongoose.Types.ObjectId, ref: 'Business', required: true},
+    IsActive: { type: Boolean, default: true }
 });
 
 export default mongoose.model('ProductCategory', ProductCategorySchema)
