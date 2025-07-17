c76fc05b4b59c90581bda1f11f91146b45747c7d
OxeAlex23 <souzaalexciano@gmail.com>
2025-07-17 20:05:26 -0300

    Update EmployeeSchema.js

--- models/EmployeeSchema.js
+++ models/EmployeeSchema.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const EmployeeSchema = new mongoose.Schema({
     EmployeeName: { type: String, required: true },
     JobTitle: { type: String, required: true },
+    EmployeeImgUrl: { type: String, required: true }
 });
 
 export default mongoose.model('Employee', EmployeeSchema);
