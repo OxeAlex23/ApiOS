3bef4e39b27df0b409df98db6fcb1cfb33344cd6
OxeAlex23 <souzaalexciano@gmail.com>
2025-09-13 23:49:29 -0300

    Refactor user authentication and registration routes
    Separated authentication logic into a new /auth endpoint and streamlined user creation in the root POST route. Improved error handling and response consistency for login and registration flows.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,142 +5,139 @@ import jwt from 'jsonwebtoken';
 import authObjectId from '../middleware/authObjectId.js';
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const { UserTypeId, FirstName, LastName, UserImgUrl, PhoneNumber, PhoneVerified, EmailAddress, EmailVerified, CPF, Role, Password, GoogleId, BusinessId } = req.body;
 
+router.post("/auth", async (req, res) => {
+  const { GoogleId, EmailAddress, Password } = req.body;
+
+  try {
     let user = await User.findOne({ EmailAddress });
+
     if (GoogleId) {
       if (user) {
-
         if (!user.GoogleId) {
           user.GoogleId = GoogleId;
           user.EmailVerified = true;
           await user.save();
         }
-      } else {
- 
-        user = await User.create({
-          UserTypeId: UserTypeId || 1,
-          FirstName,
-          LastName,
-          UserImgUrl,
-          PhoneNumber,
-          PhoneVerified,
-          EmailAddress,
-          EmailVerified: true,
-          CPF,
-          Role: Role || "client",
-          GoogleId,
-          BusinessId
-        });
       }
-
-      const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "1h" });
-      return res.status(200).json({ msg: "login Google successfully!", user, token: token });
+      const token = await jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "1h" })
+      return res.status(201).json({ msg: 'login successfully!', token: token });
     }
 
     if (Password) {
       if (user) {
-
         const isMatch = await bcrypt.compare(Password, user.Password || "");
         if (!isMatch) {
           return res.status(401).json({ msg: "incorrect credentials!" });
         }
-
-        const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "1h" });
-        return res.status(200).json({ msg: "login successfully!", token: token });
-      } else {
-        
-        const salt = 10;
-        const hashPassword = await bcrypt.hash(Password, salt);
-
-        user = await User.create({
-          UserTypeId,
-          FirstName,
-          LastName,
-          UserImgUrl,
-          PhoneNumber,
-          PhoneVerified,
-          EmailAddress,
-          EmailVerified,
-          CPF,
-          Role,
-          Password: hashPassword,
-          BusinessId
-        });
-
-        const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "1h" });
-        return res.status(201).json({ msg: "user created successfully !", user, token: token });
       }
+
+      const token = await jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "1h" });
+      return res.status(201).json({ msg: 'login successfully!', token: token });
     }
 
-    return res.status(400).json({ msg: "enter a login method!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+router.post("/", async (req, res) => {
+  try {
+    const { UserTypeId, FirstName, LastName, UserImgUrl, PhoneNumber, PhoneVerified, EmailAddress, EmailVerified, CPF, Role, Password, GoogleId, BusinessId } = req.body;
+
+    const userData = {
+      UserTypeId: UserTypeId || 1,
+      FirstName,
+      LastName,
+      UserImgUrl,
+      PhoneNumber,
+      PhoneVerified,
+      EmailAddress,
+      EmailVerified: true,
+      CPF,
+      Role: Role || "client",
+      GoogleId,
+      BusinessId
+    }
+
+    if (Password) {
+      const salt = 10;
+      const PasswordHash = await bcrypt.hash(Password, salt);
+      userData.Password = PasswordHash;
+    }
+
+    const user = await User.create(userData);
+
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "1h" });
+    return res.status(200).json({ msg: "login Google successfully!", user, token: token });
+
+
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
 });
 
 router.get('/', async (req, res) => {
-    const users = await User.find().select('-Password');
-    res.json(users);
+  const users = await User.find().select('-Password');
+  res.json(users);
 });
 
 router.get('/:id', authObjectId, async (req, res) => {
-    const userId = req.params.id;
+  const userId = req.params.id;
 
-    try {
-        const user = await User.findById(userId, '-Password');
-        if (!user) {
-            return res.status(404).json({ msg: 'user Not Found!' });
-        }
-        res.json(user);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  try {
+    const user = await User.findById(userId, '-Password');
+    if (!user) {
+      return res.status(404).json({ msg: 'user Not Found!' });
     }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 
 router.put('/:id', authObjectId, async (req, res) => {
-    const userId = req.params.id;
+  const userId = req.params.id;
 
-    try {
-        const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
-        if (!updatedUser) {
-            return res.status(404).json({ msg: 'user Not Found!' });
-        }
-        res.status(200).json(updatedUser);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ msg: 'user Not Found!' });
     }
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 router.patch('/:id', authObjectId, async (req, res) => {
-    const userId = req.params.id;
-    const { Password } = req.body;
+  const userId = req.params.id;
+  const { Password } = req.body;
 
-    try {
-        const salt = 10;
-        const hashPassword = await bcrypt.hash(Password, salt);
+  try {
+    const salt = 10;
+    const hashPassword = await bcrypt.hash(Password, salt);
 
-        const updatedUser = await User.findByIdAndUpdate(userId, { Password: hashPassword }, { new: true });
-        if (!updatedUser) {
-            return res.status(404).json({ msg: 'user Not Found!' });
-        }
-        res.status(200).json(updatedUser);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    const updatedUser = await User.findByIdAndUpdate(userId, { Password: hashPassword }, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ msg: 'user Not Found!' });
     }
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 router.delete('/:id', authObjectId, async (req, res) => {
 
-    const userDeleted = await User.findByIdAndDelete(req.params.id);
+  const userDeleted = await User.findByIdAndDelete(req.params.id);
 
-    if (!userDeleted) {
-        res.status(404).json({ msg: 'user Not Found!' })
-    }
-    res.status(200).json({ msg: 'user deleted successfully!' });
+  if (!userDeleted) {
+    res.status(404).json({ msg: 'user Not Found!' })
+  }
+  res.status(200).json({ msg: 'user deleted successfully!' });
 });
 
 export default router;
